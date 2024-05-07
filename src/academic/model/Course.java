63e8ad8d37b65394778cf11b672cce041c6ce7a9
wilsonsihombing <112602489+wilsonsihombing@users.noreply.github.com>
2024-05-07 17:36:09 +0700

    telah menyelesaika tugas

--- src/academic/model/Course.java
+++ src/academic/model/Course.java
@@ -1,11 +1,39 @@
 package academic.model;
 
-/**
- * @author NIM Nama
- * @author NIM Nama
- */
-public class Course {
+import java.util.ArrayList;
 
-    // class definition
+/**
+ * @author 12S22011 Wilson Eksaudi Sihombing
+ * @author 
+ */ 
+public class Course<T extends I_AcademicEntity>{
+ 
+    private String course_id;
+    private String course_name;
+    private int sks;
+    private String grade;
+
+    public Course(String course_id, String course_name, int sks, String grade) {
+        this.course_id = course_id;
+        this.course_name = course_name;
+        this.sks = sks;
+        this.grade = grade;
+    }
+
+    public String getCourse_id() {
+        return this.course_id;
+    }
+
+    public String getCourse_name() {
+        return this.course_name;
+    } 
+
+    public int getSks() {
+        return this.sks;
+    }
+
+    public String getGrade() {
+        return this.grade;
+    }
 
 }
